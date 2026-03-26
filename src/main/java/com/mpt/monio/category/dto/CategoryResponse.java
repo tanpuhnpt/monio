a2776462b60c96fec6428b1bfc1b10271770b3d1
tanpuh <3122411191@sv.sgu.edu.vn>
2026-03-27 01:48:32 +0700

    #4 - optimize query to get category

--- src/main/java/com/mpt/monio/category/dto/CategoryResponse.java
+++ src/main/java/com/mpt/monio/category/dto/CategoryResponse.java
@@ -8,9 +8,7 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-
 public class CategoryResponse {
     Long id;
     String name;
-    boolean isActive;
 }
