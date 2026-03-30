732144f8451433ab69022e155c5e5e9006ce3dbf
tanpuh <3122411191@sv.sgu.edu.vn>
2026-03-30 22:53:30 +0700

    restructure frontend folder

--- BE/src/main/java/com/mpt/monio/auth/repo/InvalidatedTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mpt.monio.auth.repo;
-
-import com.mpt.monio.auth.entity.InvalidatedToken;
-import org.springframework.data.repository.CrudRepository;
-
-public interface InvalidatedTokenRepository extends CrudRepository<InvalidatedToken, String> {
-}
