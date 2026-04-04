cecefe108d365a33a6ddd775e7f1e6ea241ba595
HenryNguyen1703 <nguyenngochieu170302004@gmail.com>
2026-04-04 23:09:47 +0700

    #35 - modified main

--- ai/main.py
+++ ai/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.api import expense, ocr
+from app.core.database import engine, Base
 
 app = FastAPI(title="Personal Financial Management AI", version="1.0")
+Base.metadata.create_all(bind=engine)
 app.include_router(ocr.router, tags=["OCR"])
 app.include_router(expense.router, tags=["Expense"])
